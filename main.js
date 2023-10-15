5c67317dabde163db73f0f0e2e449911f0959397
Yevhen <lashchenco@gmail.com>
2023-10-15 20:51:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var currentSlide;
+    var slidesCount;
+    var sliderCounter = document.createElement("div");
+    sliderCounter.classList.add("slider__counter");
+  
+    var updateSliderCounter = function (slick, currentIndex) {
+      currentSlide = slick.slickCurrentSlide() + 1;
+      slidesCount = slick.slideCount;
+      $(sliderCounter).text(currentSlide + "/" + slidesCount);
+    };
+  
+    $(".slider").on("init", function (event, slick) {
+      $(".slider").append(sliderCounter);
+      updateSliderCounter(slick);
+    });
+  
+    $(".slider").on("afterChange", function (event, slick, currentSlide) {
+      updateSliderCounter(slick, currentSlide);
+    });
+  
+    $(".slider").slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+      centerMode: true,
+      centerPadding: "50px",
+    });
+    $(".slick-prev").text("❯");
+    $(".slick-next").text("❯");
+  });
